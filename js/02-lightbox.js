50132c30fd5fa21dcf0849307cf8b7b9bf73b2e8
Głocki Kacper <144497395+KacpeR220199@users.noreply.github.com>
2024-02-18 10:55:29 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryBox = (galleryItems) =>
+  galleryItems
+    .map(
+      (galleryItem) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"</img>
+  </a>
+  </li>`
+    )
+    .join("");
+const createGalleryBox = galleryBox(galleryItems);
+gallery.innerHTML = createGalleryBox;
 
-console.log(galleryItems);
+let lightbox = new SimpleLightbox(".gallery__link");
+gallery.on("show.simplelightbox", function () {
+  // do something…
+});
