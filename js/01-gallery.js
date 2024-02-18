bf479326d1716285f0ed99a48f511f8a9e589854
Głocki Kacper <144497395+KacpeR220199@users.noreply.github.com>
2024-02-18 09:42:31 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryBox = (galleryItems) =>
+  galleryItems
+    .map(
+      (galleryItem) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"</img>
+  </a>
+  </li>`
+    )
+    .join("");
+const createGalleryBox = galleryBox(galleryItems);
+gallery.innerHTML = createGalleryBox;
 
-console.log(galleryItems);
+gallery.addEventListener("click", isClick);
+
+const lightbox = basicLightbox.create(
+  `<img class="big__img" src="" width="1280">`,
+  {
+    onShow: () => window.addEventListener("keydown", onEscape),
+    onClose: () => window.removeEventListener("keydown", onEscape),
+  }
+);
+function isClick(event) {
+  event.preventDefault();
+  const dataSource = event.target.dataset.source;
+  if (!dataSource) return;
+  lightbox.element().querySelector("img").src = dataSource;
+  lightbox.show();
+}
+function onEscape(event) {
+  if (event.key !== "Escape") return;
+  lightbox.close();
+}
